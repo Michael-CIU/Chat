5d6f314613fc46eb60ae84c1bf2e7f4ae298810e
Michael-CIU <32347233+Michael-CIU@users.noreply.github.com>
2018-01-09 16:28:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a207_1/chat/Chat.java
@@ -0,0 +1,47 @@
+package com.example.a207_1.chat;
+
+import com.google.firebase.database.DatabaseReference;
+    import com.google.firebase.database.FirebaseDatabase;
+
+    /**
+     * Created by Lenovo on 27/12/2017.
+     */
+
+    public class Chat {
+        private User sender;
+        private String pesan;
+        private Long tanggal;
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference chatRef = database.getReference("chats");
+
+        public void send(){
+            chatRef.push().setValue(this);
+        }
+
+
+        public User getSender() {
+            return sender;
+        }
+
+        public void setSender(User sender) {
+            this.sender = sender;
+        }
+
+        public String getPesan() {
+            return pesan;
+        }
+
+        public void setPesan(String pesan) {
+            this.pesan = pesan;
+        }
+
+        public Long getTanggal() {
+            return tanggal;
+        }
+
+        public void setTanggal(Long tanggal) {
+            this.tanggal = tanggal;
+        }
+    }
+
---
+++ app/src/main/java/com/example/a207_1/chat/RegisterActivity.java
@@ -0,0 +1,55 @@
+package com.example.lenovo.mychats;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.a207_1.chat.MainActivity;
+import com.example.a207_1.chat.R;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    com.example.lenovo.mychats.User user;
+    EditText et_nama, et_nohpregis, et_email;
+    Button bt_regis;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        et_nama = (EditText) findViewById(R.id.et_nama);
+        et_nohpregis = (EditText) findViewById(R.id.et_nohpregis);
+        et_email = (EditText) findViewById(R.id.et_email);
+        bt_regis = (Button) findViewById(R.id.bt_regis);
+
+        bt_regis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user = new com.example.lenovo.mychats.User();
+                user.setNama(et_nama.getText().toString());
+                user.setTelepon(et_nohpregis.getText().toString());
+                user.setEmail(et_email.getText().toString());
+
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference userRef = database.getReference("users");
+
+                userRef.child(user.getTelepon()).setValue(user, new DatabaseReference.CompletionListener() {
+                    @Override
+                    public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                        Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
+                        startActivity(intent);
+                    }
+                });
+            }
+        });
+
+    }
+}
